db583ffa60b9be80b244d2af6ac6d21f28c29718
Reed Sloan <sloanmobile@gmail.com>
2023-10-30 19:13:27 -0700

    Fixed an issue where the date would not update as expected when saving an inspection.

--- app/src/main/java/com/reedsloan/beekeepingapp/data/local/hive/WindSpeed.kt
+++ app/src/main/java/com/reedsloan/beekeepingapp/data/local/hive/WindSpeed.kt
@@ -1,8 +1,8 @@
 package com.reedsloan.beekeepingapp.data.local.hive
 
 enum class WindSpeed(val displayValue: String) {
-    HIGH("High"),
-    MEDIUM("Medium"),
-    LOW("Low"),
     CALM("Calm"),
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High"),
 }
