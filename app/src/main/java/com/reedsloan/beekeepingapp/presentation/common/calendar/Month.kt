249198daf69a7b3c11867c77b09d38436bfc50c0
Reed <sloanmobile@gmail.com>
2024-02-23 16:11:18 -0800

    Update HiveViewModel method to reset the inspection date to today.

--- app/src/main/java/com/reedsloan/beekeepingapp/presentation/common/calendar/Month.kt
+++ app/src/main/java/com/reedsloan/beekeepingapp/presentation/common/calendar/Month.kt
@@ -1,23 +1,55 @@
 package com.reedsloan.beekeepingapp.presentation.common.calendar
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBackIos
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.kizitonwose.calendar.core.CalendarMonth
 
 @Composable
-fun Month(month: CalendarMonth) {
-    Text(
-        text = "${month.yearMonth.month.name} ${month.yearMonth.year}",
-        style = MaterialTheme.typography.titleLarge,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 16.dp),
-        textAlign = TextAlign.Center
-    )
+fun Month(month: CalendarMonth, arrowBack: () -> Unit, arrowForward: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // arrow back
+        IconButton(
+            onClick = {
+                arrowBack()
+            }
+        ) {
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowBackIos, contentDescription = "Arrow Forward Ios"
+            )
+        }
+        Text(
+            text = "${month.yearMonth.month.name} ${month.yearMonth.year}",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Center
+        )
+        // arrow forward
+        IconButton(
+            onClick = {
+                arrowForward()
+            }
+        ) {
+            Icon(
+                Icons.AutoMirrored.Filled.ArrowForwardIos, contentDescription = "Arrow Forward Ios"
+            )
+        }
+    }
 }
